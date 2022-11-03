a660e857ad8ab29add67f2fddf27fc5f1a840455
cromega <cr.jrg08@gmail.com>
2022-11-03 14:35:24 -0500

    pulish

--- bin_to_dec.exs
+++ bin_to_dec.exs
@@ -16,11 +16,11 @@
 
 defmodule App do
 	def run do
-		bin = String.trim(IO.gets("To exit, enter without input\n\nBinary: "))
+		bin = String.trim(IO.gets("\nTo exit, enter without input\n\nBinary: "))
 		cond do
 			bin === "" -> System.halt()
 
-			String.match?(bin, ~r/[a-zA-Z]{1,}|[2-9]{1,}|[[:blank:]]{1,}/sm) -> IO.puts("Invalid input, only 0 and 1 are allowed\n")
+			String.match?(bin, ~r/[a-zA-Z]{1,}|[2-9]{1,}|[[:blank:]]{1,}/sm) -> IO.puts("\nInvalid input, only 0 and 1 are allowed\n")
 
 			String.length(bin) > 8 -> IO.puts("The max. digits to handle are 8 (ex. 10101010)\n")
 
@@ -29,7 +29,7 @@ defmodule App do
 				dec = String.to_integer(bin, 2)
 		  		IO.puts("Decimal: #{dec}\n")
 
-			true -> IO.puts("Something went wrong\n")
+			true -> IO.puts("\nSomething went wrong\n")
 		end
 		System.shell("clear")
 		App.run()
